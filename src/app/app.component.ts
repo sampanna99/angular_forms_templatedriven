ee274433c6117cd4f482751f8ad1ad4aafc20dc3
sampanna pokhrel <sampanna99@gmail.com>
2018-07-10 21:42:18 -0400

    last for template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,44 @@ export class AppComponent {
 defaultQuestion = 'pet';
 answer = '';
 genders = ['male', 'female'];
+user = {
+  username: '',
+  email: '',
+  secretQuestion: '',
+  answer: '',
+  gender: ''
+};
+submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupform.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupform.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
 //   onSubmit(form: NgForm){
 // console.log(form);
 //   }
 onSubmit(){
-  console.log(this.signupform);
+  this.submitted = true;
+  this.user.username = this.signupform.value.userData.username;
+ this.user.email = this.signupform.value.userData.email;
+  this.user.secretQuestion = this.signupform.value.secret;
+  this.user.answer = this.signupform.value.questionAnswer;
+  this.user.gender = this.signupform.value.gender;
+
+this.signupform.reset();
+  // console.log(this.signupform);
 }
 }
